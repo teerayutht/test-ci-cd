aae5c5349a5a6022757558d0dd86c1ac2453e228
teerayutht <149047162+teerayutht@users.noreply.github.com>
2024-08-09 14:56:58 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
+
 }
 
 module.exports = nextConfig
