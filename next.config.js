aae3bf02f2b4215f4be948b4273e619869cb2edd
teerayutht <149047162+teerayutht@users.noreply.github.com>
2024-08-09 15:17:28 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/test-ci-cd/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
